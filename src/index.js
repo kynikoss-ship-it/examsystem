3665ab481186459a78260d7b5d094bc901edba24
kynikoss-ship-it <kynikoss@gmail.com>
2026-05-07 10:50:32 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
-
-
-② src/index.js
-
-React 애플리케이션의 진입점 파일입니다.
-
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./styles.css";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
